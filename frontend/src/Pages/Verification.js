5e758244b8b09a2ee1aba6a5b39d3e05061b8cd6
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2023-12-19 10:29:42 +0530

    Update Verification.js

--- frontend/src/Pages/Verification.js
+++ frontend/src/Pages/Verification.js
@@ -140,7 +140,7 @@ function Verifications() {
                     <button className="continueBtn" onClick={sendOtp}>
                         Continue
                     </button>
-                    <div id="recaptcha" style={{ marginTop: '30px', marginLeft: '12px' }}></div>
+                    <div id="recaptcha" style={{ marginTop: '30px', marginLeft: '9px' }}></div>
                     {phoneNumberExists && (
                         <p style={{ color: 'black' }}>{verMessage}</p>
                     )}
