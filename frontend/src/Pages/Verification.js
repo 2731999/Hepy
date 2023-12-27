379b6d87105a9e789b6e9ea0ee10d261530d57ac
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2023-12-27 18:04:52 +0530

    Add files via upload

--- frontend/src/Pages/Verification.js
+++ frontend/src/Pages/Verification.js
@@ -188,7 +188,7 @@ function Verifications() {
                     <button className="verContinueBtn" onClick={sendOtp}>
                         Continue
                     </button>
-                    <div id="recaptcha" style={{ marginTop: '30px', marginLeft: '9px' }}></div>
+                    <div id="recaptcha" style={{ marginTop: '30px', marginLeft: '12px' }}></div>
                     {phoneNumberExists && (
                         <p style={{ color: 'black' }}>{verMessage}</p>
                     )}
