c71833a15cb7ac0f0441f9e3086d8f5c5e729e8c
Abhishek kumar <101728039+2731999@users.noreply.github.com>
2024-02-15 13:18:20 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,17 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const http = require("http");
 const socketIo = require("socket.io");
+const multer = require('multer'); 
+const storage = multer.memoryStorage();
+const mongoose = require('mongoose');
+const upload = multer({ storage: storage });
+const bodyParser = require('body-parser');
 const uri = "mongodb+srv://hepyapp:Hepy12345!@cluster0.51e0pcz.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -35,6 +41,45 @@ io.on("connection", (socket) => {
     });
 });
 
+const userSchema = new mongoose.Schema({
+    picturePath: String,
+});
+
+
+// -----------------------------------------routes-------------------------------------------------------
+
+app.post('/postFeedImg', upload.single('image'), async (req, res) => {
+    const { userId, content } = req.body;
+
+    try {
+        await client.connect();
+        const database = client.db('hepy-data');
+        const users = database.collection('users');
+
+        const query = { user_id: userId };
+
+        const updateDocument = {
+            $push: {
+                posts: {
+                    content: content,
+                    image: {
+                        data: req.file.buffer,
+                        contentType: req.file.mimetype,
+                    },
+                },
+            },
+        };
+
+        const updatedUser = await users.updateOne(query, updateDocument);
+
+        res.json(updatedUser);
+    } catch (err) {
+        console.error('Error posting:', err);
+        res.status(500).json('Internal Server Error');
+    } finally {
+        await client.close();
+    }
+});
 
 app.get('/', (req, res) => {
     res.json("API is running");
@@ -325,21 +370,269 @@ app.put('/addmatch', async (req, res) => {
         await client.connect();
         const database = client.db('hepy-data');
         const users = database.collection('users');
-        const query = { user_id: userId, likedProfiles: { $ne: matchedUserId } };
-        const updateDocument = {
-            $push: { likedProfiles: matchedUserId }
-        };
-        const updatedUser = await users.findOneAndUpdate(query, updateDocument, { returnOriginal: false });
 
-        res.send(updatedUser.value);
+        // Get information about the user who is liking
+        const liker = await users.findOne({ user_id: userId });
+
+        if (!liker) {
+            return res.status(404).json({ error: 'Liker not found' });
+        }
+
+        // Update the liked user's document
+        const likedUserQuery = { user_id: matchedUserId };
+        const likedUser = await users.findOne(likedUserQuery);
+
+        if (!likedUser) {
+            return res.status(404).json({ error: 'Liked user not found' });
+        }
+
+        const likedUserUpdateDocument = {
+            $push: {
+                beingLiked: {
+                    userId,
+                    userName: liker.first_name + ' ' + liker.last_name,
+                    pic: liker.Pic[0]
+                }
+            }
+        };
+
+        const updatedLikedUser = await users.findOneAndUpdate(
+            likedUserQuery,
+            likedUserUpdateDocument,
+            { returnDocument: 'after' }
+        );
+
+        // Update the liker's document
+        const likerQuery = { user_id: userId, 'likedProfiles.user_id': { $ne: matchedUserId } };
+        const likerUpdateDocument = {
+            $push: {
+                likedProfiles: {
+                    user_id: matchedUserId,
+                    userName: likedUser.first_name + ' ' + likedUser.last_name,
+                    pic: likedUser.Pic[0]
+                }
+            }
+        };
+
+        const updatedLiker = await users.findOneAndUpdate(
+            likerQuery,
+            likerUpdateDocument,
+            { returnDocument: 'after' }
+        );
+
+        if (!updatedLiker) {
+            return res.status(404).json({ error: 'Liker not found or already liked' });
+        }
+
+        res.json({ success: true });
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(500).json('Internal Server Error');
     } finally {
         await client.close();
     }
 });
 
+// Add a new endpoint to get subscription data by userId
+app.get('/getSubscription/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        const client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        await client.connect();
+        const database = client.db('hepy-data');
+        const subscriptionCollection = database.collection('subscription');
+
+        const subscriptionData = await subscriptionCollection.findOne({ userId });
+
+        if (subscriptionData) {
+            res.status(200).json({ success: true, data: subscriptionData });
+        } else {
+            res.status(404).json({ success: false, message: 'Subscription data not found' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/subscribe', async (req, res) => {
+    const { userId, selectedPlan, selectedTime } = req.body;
+
+    try {
+        const client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        await client.connect();
+        const database = client.db('hepy-data');
+        const subscriptionCollection = database.collection('subscription'); 
+
+        const subscriptionData = {
+            userId,
+            selectedPlan,
+            selectedTime,
+            timestamp: new Date(), 
+        };
+
+        await subscriptionCollection.insertOne(subscriptionData);
+
+        res.status(200).json({ success: true, message: 'Subscription saved successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/postFeedsUpload', upload.single('image'), async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db('hepy-data');
+        const posts = database.collection('posts');
+        const users = database.collection('users');
+
+        const user = await users.findOne({ user_id: req.body.user_id });
+
+        const newPost = {
+            user_id: req.body.user_id,
+            content: req.body.content,
+            likes: 0,
+            comments: [],
+            image: req.file.buffer,
+            userName: user ? `${user.first_name} ${user.last_name}` : 'Unknown',
+            userPic: user ? user.Pic[0] : '',
+            timestamp: new Date(),
+        };
+
+        const insertedPost = await posts.insertOne(newPost);
+
+        res.json(insertedPost);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/allPosts', async (req, res) => {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db('hepy-data');
+        const posts = database.collection('posts');
+
+        const allPosts = await posts.find({}).toArray();
+
+        res.json(allPosts);
+    } finally {
+        await client.close();
+    }
+});
+
+// --------Like Post-------------
+app.post('/likePost/:postId', async (req, res) => {
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        const database = client.db('hepy-data');
+        const posts = database.collection('posts');
+
+        const postId = req.params.postId;
+        const updatedPost = await posts.findOneAndUpdate(
+            { _id: new ObjectId(postId) },
+            { $inc: { likes: 1 } },
+            { returnDocument: 'after' }
+        );
+
+        res.json(updatedPost.value);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+});
+
+// Unlike a Post
+app.post('/unlikePost/:postId', async (req, res) => {
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        const database = client.db('hepy-data');
+        const posts = database.collection('posts');
+
+        const postId = req.params.postId;
+        const updatedPost = await posts.findOneAndUpdate(
+            { _id: new ObjectId(postId) },
+            { $inc: { likes: -1 } }, // Decrement likes by 1
+            { returnDocument: 'after' }
+        );
+
+        res.json(updatedPost.value);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+});
+
+// Add the 'saveComment' endpoint
+app.post('/saveComment/:postId', async (req, res) => {
+    const postId = req.params.postId;
+    const { userId, commentContent } = req.body;
+
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        const database = client.db('hepy-data');
+        const posts = database.collection('posts');
+        const users = database.collection('users');
+
+        const post = await posts.findOne({ _id: new ObjectId(postId) });
+        const user = await users.findOne({ user_id: userId });
+
+        if (!post) {
+            return res.status(404).json({ error: 'Post not found' });
+        }
+
+        const comment = {
+            userId,
+            userName: user ? `${user.first_name} ${user.last_name}` : 'Unknown',
+            content: commentContent,
+            timestamp: new Date(),
+        };
+
+        post.comments.push(comment);
+
+        const updatedPost = await posts.findOneAndUpdate(
+            { _id: new ObjectId(postId) },
+            { $set: { comments: post.comments } },
+            { returnDocument: 'after' }
+        );
+
+        res.status(201).json(updatedPost.value);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+});
 
 // Get Messages by from_userId and to_userId
 app.get('/messages', async (req, res) => {
@@ -395,14 +688,40 @@ app.post('/message', async (req, res) => {
 });
 
 
+// ------ Audio Saving ---------
+app.post('/upload-audio', upload.single('audio'), async (req, res) => {
+    try {
+        const client = new MongoClient(uri);
+        await client.connect();
+
+        const database = client.db('hepy-data');
+        const audioCollection = database.collection('audio');
+
+        const audioData = {
+            user_id: req.body.user_id, // Assuming you send the user_id along with the audio
+            audio: {
+                data: req.file.buffer,
+                contentType: req.file.mimetype,
+            },
+        };
+
+        const result = await audioCollection.insertOne(audioData);
+
+        res.status(201).json({ message: 'Audio recording successfully saved in the database' });
+    } catch (err) {
+        console.error('Error saving audio recording:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        await client.close();
+    }
+}); 
+
 // Update a User in the Database
 app.put('/user', async (req, res) => {
     const client = new MongoClient(uri)
     const formData = req.body.formData
 
     console.log('Received FormData:', formData); // Add this line for debugging
-
-    console.log(formData)
 
     try {
         await client.connect()
